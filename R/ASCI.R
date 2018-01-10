23317442ec3382bb12add6ffaf1e55449ec8a61e
fawda123 <mbafs2012@gmail.com>
2018-01-10 14:50:25 -0800

    working on removing OE stuff

--- R/ASCI.R
+++ R/ASCI.R
@@ -6,7 +6,7 @@
 #' @param ... additional arguments passed to other funcions, e.g., \code{\link{rfpred}}
 #' 
 #' @details 
-#' Two indices for three taxonomy types are scored, pMMI and O/E for diatoms, soft-bodied algae, and hybrid. This function combines output from the \code{\link{pmmifun}} and \code{\link{oefun}} functions in a user-friendly format.
+#' Three taxonomy types are scored for the pMMI: diatoms, soft-bodied algae, and hybrid. This function combines output from the \code{\link{pmmifun}} function in a user-friendly format.
 #' 
 #' @return 
 #' A \code{\link{asci}} object with specific methods.  See the examples for accessing.
@@ -19,14 +19,12 @@
 #' @import purrr
 #' @import tibble
 #' 
-#' @seealso  \code{\link{pmmifun}}, \code{\link{oefun}}
+#' @seealso  \code{\link{pmmifun}}
 #' 
 #' @examples 
 #' results <- ASCI(demo_algae_tax, demo_algae_sitedata)
 #' scores(results)
 #' Supp1_mmi(results)
-#' Supp1_OE(results)
-#' Supp2_OE(results)
 ASCI <- function(taxain, sitein, tax = c('diatoms', 'sba', 'hybrid'), ...){
   
   ## sanity checks
@@ -41,27 +39,12 @@ ASCI <- function(taxain, sitein, tax = c('diatoms', 'sba', 'hybrid'), ...){
   ##
   # individual output
   
-  # oe
-  oeind <- oefun(dat$taxa, dat$site, ...)
-  
   # pmmi
   pmmind <- pmmifun(dat$taxa, dat$site, ...)
     
   ##
   # main output (scores)
 
-  # oe
-  oescr <- oeind %>% 
-    map(function(x){
-      
-      x$OE_scores %>% 
-        select(SampleID, O, E, OoverE, OoverE_Percentile) %>% 
-        gather('met', 'val', -SampleID)
-      
-      }) %>% 
-    enframe('taxa') %>% 
-    unnest
-    
   # pmmi
   pmmiscr <- pmmind %>% 
     map(~ .x$MMI_scores) %>% 
@@ -70,9 +53,9 @@ ASCI <- function(taxain, sitein, tax = c('diatoms', 'sba', 'hybrid'), ...){
     unnest
 
   # combine scrs, long format
-  scr <- bind_rows(oescr, pmmiscr) %>% 
+  scr <- pmmiscr %>% 
     spread(met, val) %>% 
-    select(taxa, SampleID, MMI, MMI_Percentile, O, E, OoverE, OoverE_Percentile)
+    select(taxa, SampleID, MMI, MMI_Percentile)
   
   ##
   # supplementary info
@@ -81,31 +64,6 @@ ASCI <- function(taxain, sitein, tax = c('diatoms', 'sba', 'hybrid'), ...){
   Supp1_mmi <- pmmind %>% 
     map(~ .x$MMI_supp) %>% 
     map(gather, 'Metric', 'Value', -SampleID) %>% 
-    enframe('taxa') %>% 
-    unnest
-  
-  # oe capture probs
-  Supp1_OE <- oeind %>% 
-    map(~ .x$Capture_Probs) %>% 
-    enframe('taxa') %>% 
-    unnest
-  
-  # oe group occurrence probs
-  Supp2_OE <- oeind %>% 
-    map(~ .x$Group_Occurrence_Probs) %>% 
-    map(gather, 'pGroup', 'Prob', -SampleID) %>% 
-    enframe('taxa') %>% 
-    unnest
-  
-  # oe null
-  null_OE <- oeind %>% 
-    map(function(x){
-      
-      x$OE_scores %>% 
-        select(SampleID, Onull, Enull, OoverE.null) %>% 
-        gather('met', 'val', -SampleID)
-      
-    }) %>% 
     enframe('taxa') %>% 
     unnest
   
@@ -118,21 +76,11 @@ ASCI <- function(taxain, sitein, tax = c('diatoms', 'sba', 'hybrid'), ...){
     Supp1_mmi <- Supp1_mmi %>% 
       filter(taxa %in% tax)
     
-    Supp1_OE <- Supp1_OE %>% 
-      filter(taxa %in% tax)
-    
-    Supp2_OE <- Supp2_OE %>% 
-      filter(taxa %in% tax)
-    
-    null_OE <- null_OE %>% 
-      filter(taxa %in% tax)
-    
   }
     
   ##
   # create asci class output
-  out <- asci(scores = scr, Supp1_mmi = Supp1_mmi, Supp1_OE = Supp1_OE, 
-             Supp2_OE = Supp2_OE, null_OE = null_OE, taxa = tax)
+  out <- asci(scores = scr, Supp1_mmi = Supp1_mmi, taxa = tax)
   
   return(out)
   
--- R/class-asci.R
+++ R/class-asci.R
@@ -4,24 +4,16 @@
 #' 
 #' @slot scores \code{data.frame} of ASCI scores
 #' @slot Supp1_mmi \code{data.frame} of supplemental MMI metric scores
-#' @slot Supp1_OE \code{data.frame} of supplemental taxonomic capture probabilities for OE 
-#' @slot Supp2_OE \code{data.frame} of supplemental group capture probabilities for OE
-#' @slot null_OE \code{data.frame} of null OE scores
 #' @slot taxa character string of taxa returned with initial function call
 #' 
 #' @examples 
 #' results <- ASCI(demo_algae_tax, demo_algae_sitedata)
 #' scores(results)
 #' Supp1_mmi(results)
-#' Supp1_OE(results)
-#' Supp2_OE(results)
 asci <- setClass('asci', 
          slots = list(
            scores = 'data.frame',
            Supp1_mmi = 'data.frame', 
-           Supp1_OE = 'data.frame', 
-           Supp2_OE = 'data.frame', 
-           null_OE = 'data.frame',
            taxa = 'character'
            )
 )
