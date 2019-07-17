038f76d33e154240915ac404a61701bf8140d306
GIS Computer <GisUser@SCCWRP2k.lan>
2019-07-17 08:40:47 -0700

    changed parameters

--- R/dec_inc_score.R
+++ R/dec_inc_score.R
@@ -1,12 +1,18 @@
-#,
-#,
-#,
-#,
 
-
-score_metric <- function(taxa, bugs.m, win.metric, omni.ref){
+#' Title
+#'
+#' @param taxa 
+#' @param bugs.m 
+#' @param results.metric 
+#' @param omni.ref 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+score_metric <- function(taxa, bugs.m, results.metric, omni.ref){
   scored <- data.frame(rowname = row.names(bugs.m))
-  foo <- which(colnames(win.metric) %in% omni.ref$Metric)
+  foo <- which(colnames(results.metric) %in% omni.ref$Metric)
   cols <- names(win.metric)[foo]
   
   for (i in cols){      # i<-"prop.Cyclotella"
