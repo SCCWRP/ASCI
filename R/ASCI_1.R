118abe43bd67b898d16a0b1b5a3444ab5b713df4
fawda123 <mbafs2012@gmail.com>
2019-07-18 09:57:26 -0700

    fixing combining of scores within asci_1

--- R/ASCI_1.R
+++ R/ASCI_1.R
@@ -40,39 +40,20 @@ ASCI_1 <- function(taxain, sitein, tax = c('diatoms', 'sba', 'hybrid'), ...){
   ##
   # individual output
   
-  # oe
-  # oeind <- oefun(dat$taxa, dat$site, ...)
-  
   # mmi
   mmind <- mmifun(dat$taxa, dat$site, ...)
   
   ##
   # main output (scores)
-  
-  # oe
-  # oescr <- oeind %>% 
-  #   map(function(x){
-  #     
-  #     x$OE_scores %>% 
-  #       select(SampleID, O, E, OoverE, OoverE_Percentile) %>% 
-  #       gather('met', 'val', -SampleID)
-  #     
-  #   }) %>% 
-  #   enframe('taxa') %>% 
-  #   unnest
-  # 
+
   # pmmi
   mmiscr <- mmind %>% 
     map(~ .x$MMI_scores) %>% 
     map(gather, 'met', 'val', -SampleID) %>% 
     enframe('taxa') %>% 
-    unnest
+    unnest %>% 
+    spread(met, val)
   
-  # combine scrs, long format
-  # scr <- bind_rows(oescr, pmmiscr) %>% 
-  #   spread(met, val) %>% 
-  #   select(taxa, SampleID, MMI, MMI_Percentile, O, E, OoverE, OoverE_Percentile)
-  # 
   ##
   # supplementary info
   
@@ -83,54 +64,20 @@ ASCI_1 <- function(taxain, sitein, tax = c('diatoms', 'sba', 'hybrid'), ...){
     enframe('taxa') %>% 
     unnest
   
-  # oe capture probs
-  # Supp1_OE <- oeind %>% 
-  #   map(~ .x$Capture_Probs) %>% 
-  #   enframe('taxa') %>% 
-  #   unnest
-  
-  # oe group occurrence probs
-  # Supp2_OE <- oeind %>% 
-  #   map(~ .x$Group_Occurrence_Probs) %>% 
-  #   map(gather, 'pGroup', 'Prob', -SampleID) %>% 
-  #   enframe('taxa') %>% 
-  #   unnest
-  
-  # oe null
-  # null_OE <- oeind %>% 
-  #   map(function(x){
-  #     
-  #     x$OE_scores %>% 
-  #       select(SampleID, Onull, Enull, OoverE.null) %>% 
-  #       gather('met', 'val', -SampleID)
-  #     
-  #   }) %>% 
-  #   enframe('taxa') %>% 
-  #   unnest
-  
   # subset taxa if needed
   if(length(tax) < 3){
     
-    scr <- scr %>% 
+    mmiscr <- mmiscr %>% 
       filter(taxa %in% tax)
     
     Supp1_mmi <- Supp1_mmi %>% 
       filter(taxa %in% tax)
     
-    # Supp1_OE <- Supp1_OE %>% 
-    #   filter(taxa %in% tax)
-    # 
-    # Supp2_OE <- Supp2_OE %>% 
-    #   filter(taxa %in% tax)
-    # 
-    # null_OE <- null_OE %>% 
-    #   filter(taxa %in% tax)
-    
   }
   
   ##
   # create asci class output
-  out <- asci(scores = scr, Supp1_mmi = Supp1_mmi, taxa = tax)
+  out <- asci(scores = mmiscr, Supp1_mmi = Supp1_mmi, taxa = tax)
   
   return(out)
   
