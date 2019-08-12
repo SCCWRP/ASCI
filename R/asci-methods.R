de88e3a6abec61b98ef7655c288b6c79f3d92145
QThi <qthi.ho@gmail.com>
2019-08-12 09:39:22 -0700

    cant get rid of this yet

---
+++ R/asci-methods.R
@@ -0,0 +1,41 @@
+#' @include class-asci.R
+NULL
+
+#' @rdname asci-class
+setMethod('show', 
+          signature = 'asci', 
+          definition = function(object){
+            
+            # defined methods from below
+            meths <- .S4methods(class = 'asci')
+            meths <-  attr(meths, 'info')$generic
+            meths <- meths[!meths %in% 'show']
+            
+            # unique samples 
+            n <- scores(object)
+            n <- unique(n$SampleID)
+            n <- length(n)
+            
+            # on screen
+            cat('An object of class', class(object), '\n')
+            cat('Scores calculated for', paste(object@taxa, collapse = ', ') , 'indices for', n, 'unique samples\n')
+            cat('Use these functions for access:', paste(meths, collapse = ', '), '\n')
+            invisible(NULL)
+            
+          })
+
+#' @param object \code{asci} object created with \code{\link{ASCI}}
+#' @rdname asci-class
+#' 
+#' @export
+setGeneric('scores', function(object) standardGeneric('scores'))
+
+#' @rdname asci-class
+setMethod('scores', 'asci', function(object) object@scores)
+
+#' @rdname asci-class
+#' @export
+setGeneric('Supp1_mmi', function(object) standardGeneric('Supp1_mmi'))
+
+#' @rdname asci-class
+setMethod('Supp1_mmi', 'asci', function(object) object@Supp1_mmi)
