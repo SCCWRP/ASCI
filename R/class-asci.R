8c4f3afe9239e53767a8368bc50073c9e3c0f23b
QThi <qthi.ho@gmail.com>
2019-08-13 09:38:52 -0700

    no meower S4

--- R/class-asci.R
+++
@@ -1,9 +0,0 @@
-
-asci <- setClass('asci', 
-         slots = list(
-           scores = 'data.frame',
-           Supp1_mmi = 'data.frame', 
-           taxa = 'character'
-           )
-         )
-
