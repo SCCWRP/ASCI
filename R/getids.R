e0acd4e12ad0d850570cfcb815aabc0591e3a9c0
fawda123 <mbafs2012@gmail.com>
2019-08-14 09:53:14 -0700

    added parse for sample id to getids

--- R/getids.R
+++ R/getids.R
@@ -3,10 +3,10 @@
 #' @description Assing unique sample id to rows of taxonomy and site data
 #'
 #' @param datin \code{data.frame} of taxonomy or site data
-#'
+#' @param concatenate logical indicating if \code{SampleID} is created or parsed 
 #' @details Assigns unique sample id based on a concatenation of StationCode, SampleDate, and Replicate, used internally in \code{\link{chkinp}} and \code{\link{ASCI}}
 #'
-#' @return The original input with a new 'SampleID' column
+#' @return The original input with a new \code{SampleID} column if \code{concatenate = TRUE}, or the original input with columns for \code{StationCode}, \code{Date}, and \code{Replicate} if \code{concatenate = FALSE}
 #' 
 #' @export
 #' 
@@ -18,13 +18,27 @@
 #' @examples
 #' getids(demo_algae_tax)
 #' 
-getids <- function(datin){
+getids <- function(datin, concatenate = T){
   
-  if(!'SampleID' %in% names(datin))
+  if(!'SampleID' %in% names(datin) & concatenate)
   
     datin <- datin %>% 
       unite('SampleID', StationCode, SampleDate, Replicate, sep = '_', remove = FALSE)
 
+  if('SampleID' %in% names(datin) & !concatenate)
+
+    datin <- datin %>%
+      mutate(
+        col = SampleID,
+        Replicate = gsub('.*_([0-9]+)$', '\\1', col),
+        Replicate = as.numeric(Replicate),
+        col = gsub('_[0-9]+$', '', col),
+        Date = gsub('.*_(.*)$', '\\1', col), 
+        Station = gsub(paste(Date, collapse = '|'), '', col), 
+        Station = gsub('_$', '', Station)
+      ) %>% 
+      dplyr::select(-col)
+        
   return(datin)
   
 }
