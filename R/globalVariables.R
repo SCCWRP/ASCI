0e0a8535e46c7b307698eaa25d9a8075d2c18f78
Susie Theroux <susanna.theroux@gmail.com>
2020-06-01 16:45:33 -0700

    updated metrics

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,42 +1,52 @@
 globalVariables(c('STE', 'Class', 'acast', 'mmilkup', 'SampleTypeCode', 'Phylum', 'Result', 'ComboResult',
-                  'melt', 'value', 'specnumber', 'ddply', 'summarize', 'NitrogenUptakeMetabolism',
-                  'prop.spp.Salinity.BF.d.RF', 'stations', 'diversity', 'OxygenRequirements', 'Salinity', 
-                  'Saprobity', 'TrophicState', 'Genus', 'Motility', 'designation', 'Salinity2', 'ZHR', 'CRUS', 
-                  'Green', 'BCG', 'BCG12', 'BCG45', 'CRUS', 'IndicatorClass_TP', 'IndicatorClass_Cu', 
-                  'IndicatorClass_DOC', 'IndicatorClass_Ref', 'IndicatorClass_TN', 'Heterocystous', 
-                  'NitrogenUptakeMetabolism2', 'SampleID', 'met', 'name', 'pnorm', 
-                  'results', 'sd', 'taxa', 'val', '.', 'MMI', 'MMI_Percentile',
+                  'melt', 'value', 'specnumber', 'ddply', 'summarize', 'stations', 'diversity',
+                  'SampleID', 'met', 'name', 'pnorm', 
+                  'results', 'sd', 'taxa', 'val', '.', 'SampleID_raw', 'mets', 'rfmods', 
+                  'MMI', 'MMI_Percentile',
                   'Replicate', 'SampleDate', 'StationCode', 'BAResult', 'RefCalMean',
                   'diaind', 'variable', 'FinalIDassigned', 'StationID', 'ave', 'cls', 'data', 'fst', 'grp', 
                   'ind', 'lm', 'mmi', 'prc_amg', 'prc_wth', 'psa', 'res_tst', 'rfmod', 'scr.x', 
                   'scr.y', 'typ', 'metest', 'Assemblage', 'rowname', 'Metric', 'FinalID', 'Met', 'Value',
-                  'grouped_id', 'richness', 'scores', 'D_ASCI', 'D_NumberTaxa',
+                  'grouped_id', 'scores', 'D_ASCI', 'D_NumberTaxa',
                   'D_ValveCount', 'H_ASCI', 'H_NumberTaxa', 'S_ASCI', 'S_Biovolume', 'S_EntityCount',
                   'S_NumberTaxa', 'SampleType', 'UnrecognizedTaxa', 
                   
-                  'cnt.spp.BCG4', 'cnt.spp.BCG45', 'cnt.spp.Green', 'cnt.spp.IndicatorClass_TP_low', 'cnt.spp.SPIspecies4', 
-                  'OxyRed.DO_30.richness', 'Salinity.BF.richness', 'Saprobic.BM.richness', 'cnt.spp.IndicatorClass_DOC_high', 
+                  # traits 
+                  'NitrogenUptakeMetabolism',
+                  'OxygenRequirements', 
+                  'Salinity', 
+                  'Saprobity', 
+                  'TrophicState', 
+                  'Genus', 
+                  'Motility', 
+                  'designation', 
+                  'Salinity2', 
+                  'ZHR', 
+                  'CRUS', 
+                  'Green', 'BCG', 'BCG12', 'BCG45', 'CRUS', 'IndicatorClass_TP', 'IndicatorClass_Cu', 
+                  'IndicatorClass_DOC', 'IndicatorClass_Ref', 'IndicatorClass_TN', 'Heterocystous', 
+                  'NitrogenUptakeMetabolism2',
                   
-                  'rfmods', 
-                  
-                  'OxyRed.DO_30.richness_pred', 'OxyRed.DO_30.richness_raw', 'Salinity.BF.richness_pred', 'Salinity.BF.richness_raw',
-                  
-                  'SampleID_raw', 'mets', 
-                  
-                  'prop.spp.BCG4_pred', 'prop.spp.BCG4_raw', 'prop.spp.SPI.species4_pred', 'prop.spp.SPIspecies4_raw',
-                  'prop.spp.BCG12_pred','prop.spp.BCG12_raw','prop.spp.Salinity.BF_pred','prop.spp.Trophic.E_pred',
-                  'prop.spp.Salinity.BF_raw','prop.spp.Trophic.E_raw',
-                  
-                  
-"prop.spp.BCG12_mod", "prop.spp.OxyReq.DO_100orDO_75_raw", "prop.spp.Salinity.BF_mod", "prop.spp.Trophic.E_mod", 
-"cnt.spp.IndicatorClass_DOC_high_raw" , "prop.spp.BCG45_raw", "prop.spp.Green_raw", 
-"OxyRed.DO_30.richness_mod","prop.spp.BCG4_mod","prop.spp.IndicatorClass_DOC_high_raw", "Salinity.BF.richness_mod", 
-                  
-"prop.spp.BCG12", "prop.spp.OxyReq.DO_100orDO_75", "prop.spp.Salinity.BF", "prop.spp.Trophic.E", "richness",
-"cnt.spp.IndicatorClass_DOC_high" , "prop.spp.BCG45", "prop.spp.Green", "richness",
-"OxyRed.DO_30.richness","prop.spp.BCG4","prop.spp.IndicatorClass_DOC_high", "Salinity.BF.richness", "richness",
- 
-                  'richness_raw'))
+                  # metrics 
+                  "cnt.spp.most.tol",
+                  "EpiRho.richness",
+                  "prop.spp.IndicatorClass_TN_low",
+                  "prop.spp.Planktonic",
+                  "prop.spp.Trophic.E",
+                  "Salinity.BF.richness",
+                  "cnt.spp.IndicatorClass_TP_high",
+                  "cnt.spp.most.tol",
+                  "EpiRho.richness",
+                  "OxyRed.DO_30.richness",
+                  "prop.spp.Planktonic",
+                  "prop.spp.Trophic.E",
+                  "prop.spp.ZHR_raw",
+                  "Salinity.BF.richness",
+                  "prop.spp.IndicatorClass_DOC_high_raw",
+                  "prop.spp.IndicatorClass_NonRef_raw",
+                  "prop.spp.IndicatorClass_TP_high_raw",
+                  "prop.spp.ZHR_raw", 
+                  "richness"))
 
 #' @importFrom stats ave lm na.omit pnorm predict runif sd t.test var
 NULL
